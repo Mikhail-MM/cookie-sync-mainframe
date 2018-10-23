61b76efe7957e9143fbc1118400182b46fcd5390
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-23 00:42:24 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+const cookieParser = require('cookie-parser')
+const serveStatic = require('serve-static')
+
+const app = express();
+
+app.use(cookieParser());
+
+app.use('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://cookie-sync-partner-1.herokuapp.com");
+  res.header("Access-Control-Allow-Headers", "Origin, partner_1_tracking_id, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.use('/', (req, res, next) => {
+	console.log("Logging Forwarded-For Client IP ", req.headers["x-forwarded-for"])
+	console.log("Logging Proxy IP ", req.ip)
+	console.log("LOGGING COOKIES: ", req.cookies)
+	if (!req.cookies['mainframe_tracking_id']) {
+		console.log('Piped Request To Mainframe Does Not Have UUID.')
+		const uniqueID = uuidv4();
+		res.setHeader('Set-Cookie', [`mainframe_tracking_id=${uniqueID}`]);
+	}
+	next();
+});
+
+app.get('/sync', (req, res, next) => {
+	console.log("Mainframe Receives Sync Request")
+	console.log("logging req.body: ", req.body)
+		res.status(200).send('Syncing Requisites')
+});
+
+app.get('*', (req, res) => {
+	console.log("Catch-All Handler")
+	res.status(200).send("All Clear Chief.")
+});
+
+
+app.use(function(err, req, res, next) {
+	console.log(err)
+	res.status(err.status || 500).send();
+});
+
+
+const port = process.env.PORT || 1000;
+
+app.listen(port);
+
+console.log(`Audience Service Host listening on ${port}`);
