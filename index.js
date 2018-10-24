d0cd545053309265ab216e4d106cd0ffb761a365
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-24 15:32:22 -0400

    sync incoming clients to database via cookies passed through by partner1 as headers

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://cookie-sync-partner-1.herokuapp.com, https://cookie-sync-audience-service.herokuapp.com");
-  res.header("Access-Control-Allow-Headers", "Origin, partner_1_tracking_id, X-Requested-With, Content-Type, Accept, x-audience-tracking-id, x-partner_1_tracking_id, x-contentFocus");
+  res.header("Access-Control-Allow-Headers", "Origin, partner_1_tracking_id, X-Requested-With, Content-Type, Accept, x-audience-tracking-id, x-partner-1-tracking-id, x-contentFocus");
   res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
   res.header("Access-Control-Allow-Credentials", true);
   next();
@@ -38,22 +38,46 @@ app.use('/', (req, res, next) => {
 	console.log("Logging Proxy IP ", req.ip)
 	console.log("LOGGING COOKIES: ", req.cookies)
 	console.log(" ")
-	// Problem: This will never stop sending a UUID - the request is bounced from partner 1, and the only cookie it should ever have is from partner 1. This will be a control via headers
-	// This does not work - we can't set cookies for this domain from an indirect piped request. Spoofing the domain just gets it ignored by the useragent
-	if (!req.cookies['mainframe_tracking_id']) {
-		console.log('Piped Request To Mainframe Does Not Have UUID.')
-		const uniqueID = uuidv4();
-		res.setHeader('Set-Cookie', [`mainframe_tracking_id=${uniqueID}`]);
-	}
 	next();
 });
 
-app.get('/sync', (req, res, next) => {
-	console.log("Mainframe Receives Sync Request")
-	console.log(" ")
-	//console.log("logging full request",  req)
-	console.log("Logging headers: ", req.headers)
-		res.status(200).send('Syncing Requisites')
+app.get('/sync', async (req, res, next) => {
+	try {
+		console.log("Mainframe Receives Sync Request")
+		console.log(" ")
+		//console.log("logging full request",  req)
+		console.log("Logging important headers:")
+		console.log(req.headers['x-audience-tracking-id'])
+		console.log(req.headers['x-partner-1-tracking-id'])
+		console.log(req.headers['x-mainframe-tracking-id'])
+
+			const updatedClient = await Client.findOneAndUpdate(
+			{ 	$or: [{
+					
+					ipRange: req.headers['x-original-ip'],
+					
+					audienceTrackingID: req.headers['x-audience-tracking-id'],
+					partner1TrackingID: req.headers['x-partner-1-tracking-id'],
+					mainframeTrackingID: req.headers['x-mainframe-tracking-id'],
+				
+				}]
+			}, {
+				
+				$addToSet: { ipRange: req.headers['x-original-ip'] },
+				
+				audienceTrackingID: req.headers['x-audience-tracking-id'],
+				partner1TrackingID: req.headers['x-partner-1-tracking-id'],
+				mainframeTrackingID: req.headers['x-mainframe-tracking-id'],
+				contentFocus: req.headers['x-contentfocus'],
+
+			}, { new: true, upsert: true })
+
+			console.log("Did we get dis client doe?")
+			console.log(updatedClient)
+
+			res.status(200).json(updatedClient)
+
+	} catch(err) { next(err) }
 });
 
 app.get('/adworks', (req, res, next) => {
