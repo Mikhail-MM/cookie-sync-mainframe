1e9390ef91a6988919bd64b9dea68af1dbb6b129
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-24 22:54:32 -0400

    Attempt multiple pipe requests as promises

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/prebid', async (req, res, next) => {
 			const uniqueID = uuidv4();
 			res.setHeader('Set-Cookie', [`mainframe_tracking_id=${uniqueID}`]);
 		}
-		const bids = await Promise.all([rp('https://cookie-sync-partner-2.herokuapp.com/bidding').json(), rp('https://cookie-sync-partner-1.herokuapp.com/bidding').json()])
+		const bids = await Promise.all([req.pipe(rp('https://cookie-sync-partner-2.herokuapp.com/bidding')).json(), req.pipe(rp('https://cookie-sync-partner-1.herokuapp.com/bidding')).json()])
 		const partner1Query =  req.headers['x-partner-1-tracking-id'] || req.cookies['partner_1_tracking_id']
 		console.log('Partner 1 Query: ', partner1Query)
 
