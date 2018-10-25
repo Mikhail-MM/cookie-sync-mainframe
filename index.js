c26c152f5027a5f8804b6989b26aee8aeed81a84
Mikhail Maslyuk <leakymee@bu.edu>
2018-10-24 22:45:54 -0400

    Pass ContentFocus Down

--- index.js
+++ index.js
@@ -85,7 +85,10 @@ app.get('/adworks', async (req, res, next) => {
 				{ partner1TrackingID: req.headers['x-partner-1-tracking-id'] || '' },
 			]
 		})
-			if (clientMatch) {
+		const tryThis = await Client.find({partner1TrackingID: partner1Query})
+		console.log(clientMatch)
+		console.log("try This: ", tryThis)
+			if (clientMatch && clientMatch.contentFocus) {
 				res.sendFile(path.join(__dirname + `/${clientMatch.contentFocus}.jpg`))
 			} else {
 				res.sendFile(path.join(__dirname + `/Unknown.jpg`))
@@ -102,6 +105,7 @@ app.get('/prebid', async (req, res, next) => {
 		const bids = await Promise.all([rp('https://cookie-sync-partner-2.herokuapp.com/bidding').json(), rp('https://cookie-sync-partner-1.herokuapp.com/bidding').json()])
 		const partner1Query =  req.headers['x-partner-1-tracking-id'] || req.cookies['partner_1_tracking_id']
 		console.log('Partner 1 Query: ', partner1Query)
+
 		const clientMatch = await Client.findOne({
 			$or: [
 				{ ipRange: req.headers['x-original-ip'] || '' },	
@@ -109,13 +113,16 @@ app.get('/prebid', async (req, res, next) => {
 				{ partner1TrackingID: partner1Query || ''},
 			]
 		})
+		const tryThis = await Client.find({partner1TrackingID: partner1Query})
+		console.log(clientMatch)
+		console.log("try This: ", tryThis)
 		const winningBid = bids.reduce((a, b) => {
 			if (a.bid > b.bid) {
 				return a
 			} else return b
 		});
 		const { origin, bid } = winningBid;
-			if (clientMatch) {
+			if (clientMatch && clientMatch.contentFocus) {
 				request(`${origin}/partnerAd/${clientMatch.contentFocus}.jpg`).pipe(res)
 			} else {
 				request(`${origin}/partnerAd/Unknown.jpg`).pipe(res)
